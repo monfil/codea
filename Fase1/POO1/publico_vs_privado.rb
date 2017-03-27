cb5c54ee2b39536defda45eb86a3b9c7d5e5b9e9
m0nfil <fabiolamonfil@gmail.com>
2017-03-27 13:50:13 -0600

    Se agregó público_vs_privado.rb

--- Fase1/POO1/publico_vs_privado.rb
+++ Fase1/POO1/publico_vs_privado.rb
@@ -10,14 +10,28 @@ class Employee
 	end
 
 	def to_s
-		print "#{name} <email: #{email}> acct: #{hide_numbers(deposit_account)}"
+		p "#{@name} <email: #{@email}> acct: #{hide_numbers(@deposit_account)}"
 	end
+
+	 def vacation_days
+    coefficient * 7  
+  end
+
+  def bonus
+    coefficient * 1000
+  end
 
 	private
 
 	def hide_numbers(deposit_account)
-		deposit_account.gsub(/\d{5}/, "*****")
+		deposit_account.delete('-').gsub(/(\d{5})(\d{4})/, '*****\2')
 	end
+
+	  def coefficient
+    coefficients = { 1 => 0...1_000, 1.2 => 1_000...2_000, 1.4 => 2_000...5_000, 1.5 => 5_000..10_000 }
+
+    coefficients.find { |coefficient, range| range.include? @salary }.first
+  end
 end
 
 # Driver code
@@ -25,3 +39,9 @@ employee = Employee.new('Juan Perez', 'juan@gmail.com', 1_800, '123-456-512')
 
 employee.to_s
 # => "Juan Perez <email: juan@gmail.com> acct: *****6512"
+
+str = "The employee information is #{employee}"
+puts str
+
+puts employee.vacation_days == 8.4
+puts employee.bonus == 1_200
