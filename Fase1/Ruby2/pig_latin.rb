e842e05dd9cc46f9da9728471f4f271a42e06d80
codeamac01 <codeamac01@gmail.com>
2017-03-28 13:25:52 -0600

    Falta eliminar caracteres en pig_latin

---
+++ Fase1/Ruby2/pig_latin.rb
@@ -0,0 +1,36 @@
+def converter_pig_latin_word(word)
+  # puts "Dame una palabra: "
+  # word = gets.chomp
+  if word[0].downcase =~ /[aeiou]/
+    word + 'way'
+  else
+    if word[1] =~ /[aeiou]/
+      consonant = word[0]
+    else
+      consonant = word[0,2]
+    end
+    word.gsub!(consonant,"")
+    consonant =~ /[A-Z]/ ?  word.capitalize + consonant.downcase + "ay" : word + consonant.downcase + "ay"
+  end
+end
+
+def converter_pig_latin_sentence(sentence)
+  new_array = []
+  sentence.gsub!(/\W/)
+  # sentence.delete!('[')
+  # sentence.delete!(']')
+  # sentence.delete!('"')
+  # sentence.delete!(',')
+  sentence.split.each {|word| new_array << converter_pig_latin_word(word)}
+  new_array.map!{|item| item.to_s}.join(" ")
+end
+
+# puts ARGV.inspect
+puts converter_pig_latin_sentence(ARGV.inspect)
+# p converter_pig_latin_word("Papaya")
+# p converter_pig_latin_word("Alfajor")
+# p converter_pig_latin_word("Chancla")
+
+# p converter_pig_latin_sentence("You don't know me")
+# p converter_pig_latin_sentence("I have a pen I have an apple")
+# p converter_pig_latin_sentence("Chichinflas y malafachas")
