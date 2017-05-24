3b7b81ceed3aa63f09adeb16e515852ba2f48079
codeamac07 <codeamac07@gmail.com>
2017-05-24 13:18:50 -0500

    Esquema De Usuarios Con Active Record terminado.

--- Fase1/BD1/seccion5/app/models/user.rb
+++ Fase1/BD1/seccion5/app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
 # Implementa los métodos y validaciones de tu modelo aquí. 
+  def name
+    "#{first_name} #{last_name}"
+  end
 
+  def age
+    today = Date.today
+    today.year - birthday.year - ((today.month > birthday.month || (today.month == birthday.month && today.day >= birthday.day)) ? 0 : 1)
+  end
+
+  validates :email, :uniqueness => true
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :age, numericality: { greater_than_or_equal_to: 18 }
+  validates :phone, :format => { :with => /(\d{3}).+(\d{3}).(\d{4})/ }
 end
