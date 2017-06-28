7a36bd94fdef804bc7b575ff7d8af9a6fc190bba
m0nfil <fabiolamonfil@gmail.com>
2017-06-28 13:36:54 -0500

    Arreglando abuelita sordita asíncrona

--- Fase2/fundamentos_web_1/seccion4/abuelita_sordita_asincrona/public/js/application.js
+++ Fase2/fundamentos_web_1/seccion4/abuelita_sordita_asincrona/public/js/application.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function(){
 	// Este código corre después de que `document` fue cargado(loaded) 
 	// completamente. 
 	// Esto garantiza que si amarramos(bind) una función a un elemento 
-	// de HTML este exista ya en la página. 
-	$("abuelita").submit(function(){
-        alert("Submitted");
-    });
+	// de HTML este exista ya en la página.
+	console.log("Está listo el documento");
+	$("#abuelita").on("submit", function(event){
+		event.preventDefault(); //JQuery toma el control
+		console.log("JQuery tomó el control");
+	});
 });
